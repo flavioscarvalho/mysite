ffe2a04235546a5000ab96a1a4b5c304a8ddb1cf
Flávio Carvalho <professorflaviosoares@gmail.com>
2024-04-13 23:16:20 -0300

    Nesta branch, está faltando no templete, algumas informações

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,15 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.PostView.as_view(), name='post_list'),
+#     path('<slug:slug>/', views.post_detail, name='post_detail'),
+# ]
+
 from django.urls import path
+from . import views
 
-from blog import views
-
-
-urlpatterns=[
-    path('', views.PostView.as_view(), name='home'),
-    path("<slug:slug>/", views.PostDetail.as_view(), name='post_detail'),
+urlpatterns = [
+    path('', views.PostView.as_view(), name='home'),  # Renomear para 'home'
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
